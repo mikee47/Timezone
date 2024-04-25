04a74f211f84279dcc6a279c91fc3110fb824555
mikee47 <mike@silyhouse.net>
2024-04-25 09:51:11 +0100

    Decode rules, print in comments

--- tools/tzinfo.py
+++ tools/tzinfo.py
@@ -3,21 +3,55 @@
 import os
 import sys
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 ZONEINFO_PATH = '/usr/share/zoneinfo'
 TZDATA_PATH = ZONEINFO_PATH + '/tzdata.zi'
 
 @dataclass
 class Rule:
-    name: str
+    from_: str
+    to: str
+    in_: str
+    on: str
+    at: str
+    save: str
+    letters: str
+
+    # def __post_init__(self):
+    #     if self.from_.startswith('mi'):
+    #         self.from_ = 0
+    #     if self.to.startswith('o'):
+    #         self.to = self.from_
+
+    def __str__(self):
+        return f'{self.from_} {self.to} - {self.in_} {self.on} {self.at} {self.save} {self.letters}'
+
+@dataclass
+class ZoneRule:
+    stdoff: str
+    rule: str
+    format: str
+    until: str
+
+    def __str__(self):
+        return f'{self.stdoff} {self.rule} {self.format} {self.until}'
+
 
 @dataclass
 class Zone:
     tzstr: str
+    rules: list[ZoneRule] = field(default_factory=list)
 
-    def update(self, fields: list[str]):
-        pass
+    def add_rule(self, fields: list[str]):
+        stdoff = fields.pop(0)
+        rule = fields.pop(0)
+        format = fields.pop(0)
+        until = fields.pop(0) if fields else None
+        rule = ZoneRule(stdoff, rule, format, until)
+        self.rules.append(rule)
+        return rule
+
 
 Region = dict[Zone]
 
@@ -28,7 +62,11 @@ class TzData:
         self.comments = []
 
     def add_rule(self, fields: list[str]) -> Rule:
-        return Rule(fields[1])
+        name = fields[1]
+        rule = self.rules[name] = Rule(fields[2], fields[3], fields[5], fields[6], fields[7], fields[8], fields[9])
+        # yr_from = int(rule.from_)
+        # yr_to = int(rule.to)
+        return rule
 
     def add_zone(self, fields: list[str]) -> Zone:
         name = fields[1]
@@ -44,6 +82,7 @@ class TzData:
         region_name, _, zone_name = name.rpartition('/')
         region = self.regions.setdefault(region_name, Region())
         zone = region[zone_name] = Zone(tzstr)
+        zone.add_rule(fields[2:])
         return zone
 
     def add_link(self, fields: list[str]):
@@ -68,7 +107,7 @@ class TzData:
                 zone = None
                 self.add_link(fields)
             elif zone:
-                zone.update(fields)
+                zone.add_rule(fields)
 
     def print(self, f, define: bool):
         print(file=f)
@@ -90,7 +129,16 @@ class TzData:
             for zone_name, zone in region.items():
                 tag = zone_name.replace('-', '_N').replace('+', '_P')
                 if define:
-                    print(f'''
+                    f.write(f'''
+  /*
+     {'/'.join([region_name, zone_name])}
+''')
+                    for zr in zone.rules:
+                        print(f'      {zr}', file=f)
+                        r = self.rules.get(zr.rule)
+                        if r:
+                            print(f'        {self.rules[zr.rule]}', file=f)
+                    print(f'''  */
   DEFINE_FSTR_LOCAL(TZNAME_{tag}, "{zone_name}")
   DEFINE_FSTR_LOCAL(TZSTR_{tag}, "{zone.tzstr}")
   const TzInfo {tag} PROGMEM {{
