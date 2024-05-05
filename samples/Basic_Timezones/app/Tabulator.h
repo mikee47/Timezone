f5a5c20c697c4451558c60afa09aac7807d00e31
mikee47 <mike@silyhouse.net>
2024-05-05 11:44:44 +0100

    Move Tabulator into separate unit

---
+++ samples/Basic_Timezones/app/Tabulator.h
@@ -0,0 +1,40 @@
+#include <Print.h>
+
+/**
+ * @brief Class to print items in aligned columns
+ */
+class Tabulator
+{
+public:
+	/**
+     * @brief Constructor
+     * @param output
+     * @param colWidth Nominal column width
+     * @param lineWidth Do not exceed this line length
+     */
+	Tabulator(Print& output, unsigned colWidth = 25, unsigned lineWidth = 100)
+		: output(output), colWidth(colWidth), lineWidth(lineWidth)
+	{
+	}
+
+	/**
+     * @brief Print a cell. May occupy more than one column.
+     */
+	void print(const String& text);
+
+	/**
+     * @brief Reset to start of first column.
+     * Call when finished or to forceably start a new row of output.
+     */
+	void println()
+	{
+		output.println();
+		column = 0;
+	}
+
+private:
+	Print& output;
+	unsigned colWidth;
+	unsigned lineWidth;
+	unsigned column{0};
+};
--- samples/Basic_Timezones/app/application.cpp
+++ samples/Basic_Timezones/app/application.cpp
@@ -2,6 +2,7 @@
 #include <Timezone.h>
 #include "tzdata.h"
 #include "tzindex.h"
+#include "Tabulator.h"
 #include <Timezone/CountryMap.h>
 #include <Timezone/ZoneTable.h>
 #include <Data/CsvReader.h>
@@ -12,48 +13,6 @@ namespace
 {
 CountryMap countries;
 ZoneTable timezones;
-
-class Tabulator
-{
-public:
-	Tabulator(Print& output, unsigned colWidth = 25, unsigned lineWidth = 100)
-		: output(output), colWidth(colWidth), lineWidth(lineWidth)
-	{
-	}
-
-	void print(const String& text)
-	{
-		unsigned startColumn{column};
-		if(column % colWidth) {
-			auto count = (column + colWidth - 1) / colWidth;
-			startColumn = count * colWidth;
-		}
-
-		auto textlen = text.length();
-		if(startColumn + textlen > lineWidth) {
-			column = 0;
-			output.println();
-		} else {
-			auto padlen = startColumn - column;
-			output.print(String().pad(padlen));
-			textlen += padlen;
-		}
-		output.print(text);
-		column += textlen;
-	}
-
-	void println()
-	{
-		output.println();
-		column = 0;
-	}
-
-private:
-	Print& output;
-	unsigned colWidth;
-	unsigned lineWidth;
-	unsigned column{0};
-};
 
 namespace Menu
 {
